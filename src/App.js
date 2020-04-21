724a5afc4b0d69ff55d8d4b1da8df9e54b669764
oladimeji <tommyslipz@gmail.com>
2020-04-21 10:15:21 +0100

    added collection page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
 
-const HatsPage = () =>(
-  <div>
-    <h1>Hats page</h1>
-  </div>
-)
 
 function App() {
   return (
     <div className="App">
        <Switch>
        <Route exact path='/' component={HomePage} />
-        <Route  path='/hats' component={HatsPage} />
+        <Route  path='/shop' component={ShopPage} />
        </Switch>
        
     </div>
